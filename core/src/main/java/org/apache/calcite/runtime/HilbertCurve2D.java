81291ca5a08f717c7fa42e8b89c114e5a9659286
Andrei Degtiarev <adegtiarev@gmail.com>
2023-12-01 16:56:45 -0300

    update

--- core/src/main/java/org/apache/calcite/runtime/HilbertCurve2D.java
+++ core/src/main/java/org/apache/calcite/runtime/HilbertCurve2D.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://calcite.apache.org/
+*    Release: https://github.com/apache/calcite/releases/tag/calcite-1.34.0-rc0
+*    Source File: HilbertCurve2D.java
+*    
+*    Copyrights:
+*      copyright (c) 2015 azavea
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
